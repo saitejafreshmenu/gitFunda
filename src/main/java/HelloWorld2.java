56149c26d44f34916c471594f683592f63d02b3a
Sai Teja Tokala <saiteja.tokala@freshmenu.com>
2016-02-13 19:33:17 +0530

    Added a new Hello World

---
+++ src/main/java/HelloWorld2.java
@@ -0,0 +1,11 @@
+/**
+ * Created by saitejatokala on 13/02/16.
+ */
+public class HelloWorld2 {
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(11);
+        System.out.println(12);
+
+    }
+}
